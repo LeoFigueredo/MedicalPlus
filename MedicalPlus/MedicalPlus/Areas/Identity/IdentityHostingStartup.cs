37faa9c8de882560a6987d1e8af3c2f8e975166e
LeoFigueredo <leonardofprada@gmail.com>
2018-07-31 00:17:55 -0400

    Scaffold Identity

---
+++ MedicalPlus/MedicalPlus/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MedicalPlus.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(MedicalPlus.Areas.Identity.IdentityHostingStartup))]
+namespace MedicalPlus.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
