636643163c456ba9b992a18eabd3c55ec8dd5db4
LeoFigueredo <leonardofprada@gmail.com>
2018-07-30 23:37:22 -0400

    Iniciando con cambios en el DBContext

---
+++ MedicalPlus/MedicalPlus/Data/MedicalPlusContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalPlus.Data
+{
+    public class MedicalPlusContext : IdentityDbContext
+    {
+        public MedicalPlusContext(DbContextOptions<MedicalPlusContext> options)
+            : base(options)
+        {
+
+         
+        }
+    }
+}
